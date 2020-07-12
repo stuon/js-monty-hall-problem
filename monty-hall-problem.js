17dc581adcdc3ac686f779ba915d96bb2cb788f4
San Tuon <san.tuon@gmail.com>
2020-07-12 15:37:52 +1000

    code refactor

--- monty-hall-problem.js
+++ monty-hall-problem.js
@@ -1,40 +1,132 @@
-const DOOR_WIDTH = 150;
-const DOOR_HEIGHT = 240;
-const DOOR_SPACE = 80;
-const COLOR_LIGHT_BLUE = "#cce6ff";
+const DOOR_WIDTH = 120;
+const DOOR_HEIGHT = 160;
+const DOOR_COLOR = "green";
+const DOOR_COUNT = 3;
 
-var canvas = document.getElementById("canvas");
+var score = 0;
+var round = 0;
 
-function drawGame() {
-  var ctx = canvas.getContext("2d");
-  nextGame(ctx);
+var gameBoard;
+var scoreBoard;
+var doors = Array(DOOR_COUNT);
+
+function startGame() {
+  gameBoard = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+      this.canvas.width = 640;
+      this.canvas.height = 480;
+      this.context = this.canvas.getContext("2d");
+      var game = document.getElementById("game");
+      game.insertBefore(this.canvas, game.childNodes[0]);
+    },
+    clear: function () {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+  };
+
+  gameBoard.start();
+  scoreBoard = new scoreBarComponent("black", 40, 40);
+
+  for (i = 0; i < DOOR_COUNT; i++) {
+    doors[i] = new doorComponent(
+      i + 1,
+      DOOR_WIDTH,
+      DOOR_HEIGHT,
+      DOOR_COLOR,
+      70 + i * (70 + DOOR_WIDTH),
+      100
+    );
+  }
+
+  updateGameArea(true);
 }
 
-function drawDoor(ctx, x, y) {
-  ctx.beginPath();
-  ctx.rect(x, y, DOOR_WIDTH, DOOR_HEIGHT);
-  ctx.fillStyle = COLOR_LIGHT_BLUE;
-  ctx.fill();
+function doorComponent(id, width, height, color, x, y) {
+  this.id = id;
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.color = color;
+  this.choose = function () {
+    ctx = gameBoard.context;
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 10;
+    ctx.strokeRect(this.x, this.y + 20, this.width, this.height);
+  };
+  this.unchoose = function () {
+    ctx = gameBoard.context;
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 10;
+    ctx.strokeRect(this.x, this.y + 20, this.width, this.height);
+  };
+  this.update = function () {
+    ctx = gameBoard.context;
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y + 20, this.width, this.height);
+    ctx.textAlign = "center";
+    ctx.fillStyle = "black";
+    ctx.font = "16px Verdana";
+    text = "Door " + id;
+    ctx.fillText(text, this.x + this.width / 2, this.y);
+
+    this.unchoose();
+  };
 }
 
-function nextGame(ctx) {
-  for (i = 0; i < 3; i++) {
-    drawDoor(ctx, i * (DOOR_WIDTH + DOOR_SPACE) + DOOR_SPACE, 50);
+function scoreBarComponent(color, x, y) {
+  this.x = x;
+  this.y = y;
+  this.color = color;
+  this.updateScore = function (score, round) {
+    ctx = gameBoard.context;
+    ctx.fillStyle = this.color;
+    ctx.font = "16px Verdana";
+    text = "Score: " + score + " / " + round;
+    ctx.fillText(text, this.x, this.y);
+  };
+}
+
+function updateGameArea(clearScore) {
+  if (clearScore) {
+    score = 0;
+    round = 0;
+  }
+  gameBoard.clear();
+  scoreBoard.updateScore(score, round);
+  for (i = 0; i < DOOR_COUNT; i++) {
+    doors[i].update();
   }
 }
 
-function reset() {
-  alert("reset");
+function chooseDoor(num) {
+  for (i = 0; i < DOOR_COUNT; i++) {
+    if (num == i) {
+      doors[i].choose();
+    } else {
+      doors[i].unchoose();
+    }
+  }
 }
 
-function openDoor1() {
-  alert("door 1");
+function restart() {
+  updateGameArea(true);
 }
 
-function openDoor2() {
-  alert("door 2");
+function door1() {
+  chooseDoor(0);
 }
 
-function openDoor3() {
-  alert("door 3");
+function door2() {
+  chooseDoor(1);
+}
+
+function door3() {
+  chooseDoor(2);
+}
+
+function nextRound() {
+  round++;
+  updateGameArea(false);
 }
