fb4c54691534fdede014ccb792465b215a5b5e2c
San Tuon <san.tuon@gmail.com>
2020-07-19 01:23:19 +1000

    fixed gamestate bug

--- js/game-state.js
+++ js/game-state.js
@@ -1,48 +1,49 @@
 "use strict";
 
-class gameState {
-  constructor() {
+function gameState() {
+  this.score = 0;
+  this.round = 0;
+  this.gameSamples = [];
+  this.chosenFirstDoor = false;
+  this.currentDoorChosen = 0;
+  this.doorShown = 0;
+  this.roundFinished = false;
+  this.autoRun = false;
+}
+
+gameState.prototype.init = function () {
+  this.clear(true);
+};
+
+gameState.prototype.clear = function (resetAll = false) {
+  if (resetAll) {
     this.score = 0;
     this.round = 0;
+    this.autoRun = false;
+
+    // Generate game for user, so we know we aren't cheating
     this.gameSamples = [];
-    this.chosenFirstDoor = false;
-    this.currentDoorChosen = 0;
-    this.doorShown = 0;
-    this.roundFinished = false;
-    this.autoRun = false;
-  }
-  init() {
-    this.clear(true);
-  }
-  clear(resetAll = false) {
-    if (resetAll) {
-      this.score = 0;
-      this.round = 0;
-      this.autoRun = false;
-
-      // Generate game for user, so we know we aren't cheating
-      this.gameSamples = [];
-      for (let i = 0; i < SAMPLES; i++) {
-        let carIndex = getRndInteger(0, 2);
-        this.gameSamples[i] = carIndex;
-      }
+    for (let i = 0; i < SAMPLES; i++) {
+      let carIndex = getRndInteger(0, 2);
+      this.gameSamples[i] = carIndex;
     }
+  }
 
-    this.gameBoard = 0;
-    this.scoreBoard = 0;
-    this.chosenFirstDoor = false;
-    this.currentDoorChosen = 0;
-    this.doorShown = 0;
-    this.roundFinished = false;
-    this.autoRun = false;
+  this.gameBoard = 0;
+  this.scoreBoard = 0;
+  this.chosenFirstDoor = false;
+  this.currentDoorChosen = 0;
+  this.doorShown = 0;
+  this.roundFinished = false;
+  this.autoRun = false;
+};
+
+gameState.prototype.FinishRound = function () {
+  if (
+    this.chosenFirstDoor &&
+    this.currentDoorChosen == this.gameSamples[this.round]
+  ) {
+    this.score++;
   }
-  FinishRound() {
-    if (
-      this.chosenFirstDoor &&
-      this.currentDoorChosen == this.gameSamples[this.round]
-    ) {
-      this.score++;
-    }
-    this.round++;
-  }
-}
+  this.round++;
+};
